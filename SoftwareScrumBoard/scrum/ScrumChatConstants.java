3b73e4238a10ee289880b7115e911eef2cc35951
Alex Ayala <aayala4@gitlab.com>
2018-11-29 10:00:37 -0500

    constants for chat

---
+++ SoftwareScrumBoard/scrum/ScrumChatConstants.java
@@ -0,0 +1,9 @@
+package scrum;
+
+public interface ScrumChatConstants {
+
+    public static int SEND_EDIT_REQUEST = 1;
+    public static int SEND_UPDATE = 2;
+    public static int GET_EDIT_REQUEST = 3;
+    public static int GET_UPDATE = 4;
+}
